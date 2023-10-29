4fdc78aaaeb078dbe2d77a3214e6be86bb837907
jeremiekunkela <jeremie.kunkela.pro@gmail.com>
2023-10-29 19:30:30 +0100

    fix archiv ebug

--- lib/screens/archive_screen.dart
+++ lib/screens/archive_screen.dart
@@ -1,117 +1,101 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:quick_notes/helper.dart';
 import 'package:quick_notes/models/note.dart';
 import 'package:quick_notes/providers/notes_provider.dart';
 
 class ArchiveScreen extends StatelessWidget {
-  final NotesModel notesModel = NotesModel();
   final List<Note> archivedNotes;
 
   ArchiveScreen(this.archivedNotes);
 
   @override
   Widget build(BuildContext context) {
+    final notesModel = Provider.of<NotesModel>(context, listen: false);
+
     return Scaffold(
-      appBar: buildAppBar(),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: const Text(
+          'Archived Notes',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+            fontStyle: FontStyle.italic,
+          ),
+        ),
+        centerTitle: true,
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.blue, Colors.purple],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+        ),
+      ),
       body: archivedNotes.isEmpty
-          ? buildEmptyArchiveView()
-          : buildArchiveListView(),
-    );
-  }
-
-  PreferredSizeWidget buildAppBar() {
-    return AppBar(
-      backgroundColor: Colors.transparent,
-      elevation: 0,
-      title: const Text(
-        'Archived Notes',
-        style: TextStyle(
-          color: Colors.white,
-          fontSize: 25,
-          fontWeight: FontWeight.bold,
-          fontStyle: FontStyle.italic,
-        ),
-      ),
-      centerTitle: true,
-      flexibleSpace: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [Colors.blue, Colors.purple],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget buildEmptyArchiveView() {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset('assets/empty_archive_image.png',
-              width: 400, height: 300),
-          const SizedBox(height: 20),
-          const Text(
-            'No archived notes',
-            style: TextStyle(fontSize: 20, color: Colors.black),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget buildArchiveListView() {
-    return ListView.builder(
-      itemCount: archivedNotes.length,
-      itemBuilder: (context, index) {
-        final note = archivedNotes[index];
-        return Dismissible(
-          key: Key(note.title),
-          onDismissed: (direction) {
-            handleDismissedItem(context, note, direction);
-          },
-          background: buildDismissBackground(
-              Icons.delete, Colors.red, Alignment.centerLeft),
-          secondaryBackground: buildDismissBackground(
-              Icons.restore, Colors.green, Alignment.centerRight),
-          child: buildNoteCard(context, note),
-        );
-      },
-    );
-  }
-
-  void handleDismissedItem(
-      BuildContext context, Note note, DismissDirection direction) {
-    if (direction == DismissDirection.startToEnd) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          duration: const Duration(seconds: 1),
-          content: Text('${note.title} supprimée des archives'),
-        ),
-      );
-      archivedNotes.remove(note);
-    } else {
-      notesModel.restoreNoteFromArchive(note);
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          duration: const Duration(seconds: 1),
-          content: Text('${note.title} restaurée'),
-        ),
-      );
-    }
-  }
-
-  Widget buildDismissBackground(
-      IconData icon, Color color, Alignment alignment) {
-    return Container(
-      color: color,
-      alignment: alignment,
-      child: Icon(
-        icon,
-        color: Colors.white,
-      ),
+          ? Center(
+              child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset('assets/empty_archive_image.png',
+                    width: 400, height: 300),
+                const SizedBox(height: 20),
+                const Text(
+                  'No archived notes',
+                  style: TextStyle(fontSize: 20, color: Colors.black),
+                ),
+              ],
+            ))
+          : ListView.builder(
+              itemCount: archivedNotes.length,
+              itemBuilder: (context, index) {
+                final note = archivedNotes[index];
+                return Dismissible(
+                  key: Key(note.title),
+                  onDismissed: (direction) {
+                    if (direction == DismissDirection.startToEnd) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          duration: const Duration(seconds: 1),
+                          content: Text('${note.title} supprimée des archives'),
+                        ),
+                      );
+                      archivedNotes.remove(note);
+                    } else {
+                      notesModel.restoreNoteFromArchive(note);
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          duration: const Duration(seconds: 1),
+                          content: Text('${note.title} restaurée'),
+                        ),
+                      );
+                    }
+                  },
+                  background: Container(
+                    color: Colors.red,
+                    alignment: Alignment.centerLeft,
+                    child: const Icon(
+                      Icons.delete,
+                      color: Colors.white,
+                    ),
+                  ),
+                  secondaryBackground: Container(
+                    color: Colors.green,
+                    alignment: Alignment.centerRight,
+                    child: const Icon(
+                      Icons.restore,
+                      color: Colors.white,
+                    ),
+                  ),
+                  child: buildNoteCard(context, note),
+                );
+              },
+            ),
     );
   }
 
